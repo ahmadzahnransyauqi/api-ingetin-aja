f75c13c41f1a48b28db884f17e860704be39353f
Ahmad Zahran Syauqi <164448324+ahmadzahnransyauqi@users.noreply.github.com>
2025-12-16 19:21:27 +0700

    last

--- src/config/database.js
+++ src/config/database.js
@@ -1,34 +1,24 @@
-const pg = require("pg");
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-let connectionString = process.env.DATABASE_URL;
-if (connectionString && connectionString.includes('?')) {
-  connectionString = connectionString.split('?')[0];
-}
-
 console.log("🔄 Mencoba koneksi ke Database...");
 
-const sequelize = new Sequelize(connectionString, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  dialectModule: pg,
   logging: false,
-  benchmark: true, 
 
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
-  
-    connectionTimeoutMillis: 30000, 
   },
 
   pool: {
     max: 1, 
     min: 0,
-    acquire: 60000,
-    idle: 5000,
+    acquire: 30000,
+    idle: 10000,
   },
 });
 
